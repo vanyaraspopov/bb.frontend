a32a741bcd8bfc6e934932a0ad12ffe5d709a41b
RV <vanyaraspopov@mail.ru>
2018-03-09 16:31:34 +0500

    Currency info added

--- js/app.js
+++ js/app.js
@@ -8,15 +8,29 @@ const API_URI = '/api';
 window.vm = new Vue({
     el: '#bb',
     data: {
+        currencies: [],
         modules: {}
     },
     methods: {
+        currencySubmit(event){
+            alert('submit');
+            let form = event.target;
+            let data = new FormData(form);
+            currencySubmit(data);
+        },
         refreshModules() {
             refreshModules(this);
         }
     },
     mounted() {
         refreshModules(this);
+        axios.get(`${API_URI}/currencies`)
+            .then(response => {
+                for (let currency of response.data) {
+                    this.currencies.push(currency);
+                }
+            })
+            .catch(err => console.error(err));
     }
 });
 
@@ -30,4 +44,8 @@ function refreshModules(vm) {
             }
         })
         .catch(error => console.error(error));
+}
+
+function currencySubmit(data) {
+    axios.get('/', data);
 }
