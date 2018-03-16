eeb53dd6208458ce9dfa63a2ad1f4f6a703624b4
RV <vanyaraspopov@mail.ru>
2018-03-17 00:43:34 +0500

    Migrating to laravel-mix

---
+++ webpack.mix.js
@@ -0,0 +1,31 @@
+const mix = require('laravel-mix');
+
+mix.options({
+    publicPath: 'dist'
+});
+
+mix.js('js/app.js', 'js')
+    .extract([
+        'axios',
+        'vue'
+    ])
+    .sourceMaps(false, 'cheap-module-eval-source-map')
+    .disableNotifications();
+
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            'axios$': 'axios/dist/axios.min.js',
+            'vue$': 'vue/dist/vue.min.js'
+        }
+    }
+});
+
+mix.browserSync({
+    files: [
+        "index.html",
+        "dist/js/*.js",
+        "css/*.css"
+    ],
+    proxy: 'localhost:3000'
+});
